ba4b6b8714c40c4bc7937ec83f7a1dbfbc0148bf
Nick Goddard <nicko777@gmail.com>
2011-05-24 22:09:48 +0100

    fixed rake

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.7'
+gem 'rake', '~> 0.8.7'
+gem 'devise'
 
 group :development do
   gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ GTDTodos::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
 
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-
   root :to => "pages#home"
 
 end

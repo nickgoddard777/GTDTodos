5c8787cd5d35475225c120c27c5e95b091440ebb
Nick Goddard <nicko777@gmail.com>
2011-05-24 20:25:09 +0100

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 GTDTodos::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ GTDTodos::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages/home"
 
   # See how all your routes lay out with "rake routes"
 
@@ -56,3 +60,4 @@ GTDTodos::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+

b5153529c8c6b1db4fb5c412cf9f205c844245bb
Nick Goddard <nicko777@gmail.com>
2011-05-24 21:07:52 +0100

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -4,12 +4,13 @@ gem 'rails', '3.0.7'
 
 group :development do
   gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
-  gem 'rspec-rails', '2.5.0'
+  gem 'rspec-rails'
 end
 
 group :test do
   gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
-  gem 'rspec', '2.5.0'
+  gem 'rspec'
   gem 'webrat', '0.7.1'
+  gem 'spork', '0.9.0.rc5'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 GTDTodos::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +52,7 @@ GTDTodos::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "pages/home"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
